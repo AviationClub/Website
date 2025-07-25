e45506ca6f3cccdfc3ca318b040c9d51cee1bfb1
Mazen <Mazenessam2011@outlook.com>
2025-07-25 19:10:07 +0300

    add variables hardcoded

--- api/submitForm/index.js
+++ api/submitForm/index.js
@@ -13,10 +13,10 @@ module.exports = async function (context, req) {
 
   try {
     const config = {
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      server: process.env.DB_SERVER, // e.g. yourserver.database.windows.net
-      database: process.env.DB_NAME,
+      user: "Aviationclub2",
+      password: "Aviation@Mazen2024",
+      server: "aviationclub.database.windows.net", // e.g. yourserver.database.windows.net
+      database: "aviationclub",
       options: {
         encrypt: true,
       },
