689ff92937e5ad0c0b64eb8c6550f9f61fd6533b
Ziad <ziadsisi79@gmail.com>
2026-04-22 06:18:19 +0200

    fix: enable Next.js server mode for API routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   reactStrictMode: false,
   typescript: {
     ignoreBuildErrors: true,
   },
 };
-
 export default nextConfig;
